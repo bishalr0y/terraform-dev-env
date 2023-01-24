c1c9abb8d38dc451dfa795bb2f95b05aba24bada
Bishal Roy <bishalroy895@gmail.com>
2023-01-24 21:23:22 +0530

    route table association

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ resource "aws_vpc" "my_vpc" {
 }
 
 # subnet
+
 resource "aws_subnet" "my_public_subnet" {
   vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.123.1.0/24"
@@ -22,6 +23,7 @@ resource "aws_subnet" "my_public_subnet" {
 
 
 # internet gateway
+
 resource "aws_internet_gateway" "my_internet_gateway" {
   vpc_id = aws_vpc.my_vpc.id
 
@@ -29,3 +31,28 @@ resource "aws_internet_gateway" "my_internet_gateway" {
     "Name" = "dev-igw"
   }
 }
+
+# route table
+
+resource "aws_route_table" "my_public_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    "Name" = "dev_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.my_public_rt.id
+  destination_cidr_block = "0.0.0.0/0" # all IP addresses
+  gateway_id             = aws_internet_gateway.my_internet_gateway.id
+}
+
+
+# route table association
+resource "aws_route_table_association" "my_public_assoc" {
+  subnet_id      = aws_subnet.my_public_subnet.id
+  route_table_id = aws_route_table.my_public_rt.id
+
+}
+
