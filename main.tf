f2f489fc5d6e65688699ca25a9073907f3afe23d
Bishal Roy <bishalroy895@gmail.com>
2023-01-24 20:33:32 +0530

    create a public subnet

--- main.tf
+++ main.tf
@@ -1,9 +1,31 @@
 # creating a VPC
-resource "aws_vpc" "mtc_vpc" { # MoreThanCertified
-  cidr_block = "10.123.0.0/16"
+resource "aws_vpc" "my_vpc" {
+  cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
 
   tags = {
     "Name" = "dev"
+  }
+}
+
+# subnet
+resource "aws_subnet" "my_public_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    "Name" = "dev-public"
+  }
+}
+
+
+# internet gateway
+resource "aws_internet_gateway" "my_internet_gateway" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    "Name" = "dev-igw"
   }
 }
