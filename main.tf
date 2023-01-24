b575bbee9c2812045ad97af428aac66ba2e6216b
Bishal Roy <bishalroy895@gmail.com>
2023-01-24 21:27:58 +0530

    add security group

--- main.tf
+++ main.tf
@@ -56,3 +56,22 @@ resource "aws_route_table_association" "my_public_assoc" {
 
 }
 
+# security group
+
+resource "aws_security_group" "my_stg" {
+  name        = "dev-sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.my_vpc.id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
